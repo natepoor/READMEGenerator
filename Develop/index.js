566ea90200aa1a68233b29ca31b77f846fa229e6
natepoor <137231803+natepoor@users.noreply.github.com>
2023-10-11 22:23:44 -0700

    Add files via upload

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What was your motivation?",
+    "Why did you build this prject?",
+    "What problem does it solve?",
+    "What did you learn?",
+    "What makes your project stand out?",
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
